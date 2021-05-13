dbae9edb5e0944930201fde06956f14823d4fb5b
udarcs <83726078+udarcs@users.noreply.github.com>
2021-05-13 21:33:32 +0300

    Add files via upload

---
+++ TableViewController.swift
@@ -0,0 +1,123 @@
+//
+//  TableViewController.swift
+//  toDoCoreData
+//
+//  Created by mikhail on 13.05.2021.
+//
+
+import UIKit
+import CoreData
+
+class TableViewController: UITableViewController {
+    
+    var tasks: [Tasks] = []
+
+    
+    @IBAction func deliteTasks(_ sender: UIBarButtonItem) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
+        
+        if let tasks = try? context.fetch(fetchRequest) {
+            for task in tasks {
+                context.delete(task)
+            }
+        }
+        
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        tableView.reloadData()
+       
+    }
+    
+    @IBAction func plusTasks(_ sender: UIBarButtonItem) {
+        
+        let alertController = UIAlertController(title: "New task", message: "Add", preferredStyle: .alert)
+        
+        let saveTask = UIAlertAction(title: "Save", style: .default) { action in
+            let tf = alertController.textFields?.first
+            if let newTask = tf?.text {
+                self.saveTask(withTitle: newTask)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alertController.addTextField { _ in }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        
+        alertController.addAction(saveTask)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    func saveTask(withTitle title: String) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Tasks", in: context) else { return }
+        
+        let taskObject = Tasks(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do {
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Tasks> = Tasks.fetchRequest()
+        
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tasks.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+
+        return cell
+    }
+
+
+}
